20232f8bdb992ce39fde9341bc3e972c9b743417
Jasper Culong <jculongit10@gmail.com>
2023-07-03 18:30:06 +0800

    initial commit from spring initializr

---
+++ src/main/kotlin/com/japzio/springbootkt/SpringBootKtApplication.kt
@@ -0,0 +1,11 @@
+package com.japzio.springbootkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/japzio/springbootkt/SpringBootKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.japzio.springbootkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
